20322256d33039c932c8deaf59170a83aac50f99
Ivan <vandoslando@gmail.com>
2023-09-03 09:11:38 +0300

    file v1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
-import testTpl from './test.handlebars';
+import countryTpl from './countries.hbs';
 
-document.getElementById('js-creater')
-.addEventListener('click', ()=>{
-    document.body.insertAdjacentHTML(
-        'beforeend', 
-        testTpl({discription: 'This is my template'})
-        )
-})
+const cardContainer = document.querySelector('.js-card-container')
+const form = document.querySelector('form')
+const requestsEl = document.querySelector('.requests')
+const input = document.querySelector('.form-control')
+
+form.addEventListener('submit', searchCard)
+
+function searchCard(e) {
+    e.preventDefault();
+    const inputValue = e.currentTarget.elements.query.value
+    cardContainer.innerHTML = ''
+    fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(response => response.json())
+    .then((country)=> {
+        console.log(country);
+        if (country.length <= 1) {
+            console.log(country[0].flags);
+            const cardMarkUp = countryTpl(country[0])
+            cardContainer.innerHTML += cardMarkUp
+        }
+    });
+}
+
+input.addEventListener('input', showRequests)
+
+function showRequests(e) {
+    requestsEl.innerHTML = ``
+    const inputValue = e.target.value
+    fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(response => response.json())
+    .then((country)=> {
+        for (let i = 0; i <= country.length; i++) {
+
+        }
+        const countryName = country[i].name.common.toLowerCase()
+        if (countryName.includes(inputValue.toLowerCase()) && inputValue !== '') {
+            if (country.length <= 10) {
+                requestsEl.innerHTML += `<li>${country[0].name.common}</li>`
+            }
+        }
+    });
+}
