6e171ce6cc8687864ed46a7880ba1124af377ae7
Ivan <vandoslando@gmail.com>
2023-09-03 09:12:08 +0300

    file v1.0

--- src/index.js
+++ src/index.js
@@ -1,42 +1,47 @@
 import countryTpl from './countries.hbs';
 
 const cardContainer = document.querySelector('.js-card-container')
-const form = document.querySelector('form')
 const requestsEl = document.querySelector('.requests')
 const input = document.querySelector('.form-control')
 
-form.addEventListener('submit', searchCard)
+input.addEventListener('input',  _.debounce(showRequests,500))
 
-function searchCard(e) {
-    e.preventDefault();
-    const inputValue = e.currentTarget.elements.query.value
-    cardContainer.innerHTML = ''
-    fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(response => response.json())
-    .then((country)=> {
-        console.log(country);
-        if (country.length <= 1) {
-            console.log(country[0].flags);
-            const cardMarkUp = countryTpl(country[0])
-            cardContainer.innerHTML += cardMarkUp
-        }
-    });
+function getData(inputValue) {
+    return fetch(`https://restcountries.com/v3.1/name/${inputValue}`)
+    .then(response => response.json())
 }
 
-input.addEventListener('input', showRequests)
+
+function searchCountryData(country) {
+    const countryLanguagesLenght = Object.values(country[0].languages).length
+    const cardMarkUp = countryTpl(country[0])
+    cardContainer.innerHTML += cardMarkUp
+    const listGroup = document.querySelector('.list-group')
+    for (let i = 0 ;i < countryLanguagesLenght; i++) {
+        listGroup.innerHTML += `<li class="list-group-item">${Object.values(country[0].languages)[i]}</li> `
+    }
+}
 
 function showRequests(e) {
-    requestsEl.innerHTML = ``
     const inputValue = e.target.value
-    fetch(`https://restcountries.com/v3.1/name/${inputValue}`).then(response => response.json())
+    cardContainer.innerHTML = ''
+    getData(inputValue)
     .then((country)=> {
-        for (let i = 0; i <= country.length; i++) {
-
-        }
-        const countryName = country[i].name.common.toLowerCase()
-        if (countryName.includes(inputValue.toLowerCase()) && inputValue !== '') {
-            if (country.length <= 10) {
-                requestsEl.innerHTML += `<li>${country[0].name.common}</li>`
+        requestsEl.innerHTML = ``
+        if (country.length <= 10) {
+            for (let i = 0; i < country.length; i++) {
+                const countryName = country[i].name.common.toLowerCase()
+                if (countryName.includes(inputValue.toLowerCase()) && inputValue !== '') {
+                    requestsEl.innerHTML += `<li>${country[i].name.common}</li>`
+                }
             }
+        } else {
+            PNotify.error({
+                text: 'Знайдено надто багато країн!',
+            });
+        }
+        if (country.length <= 1) {
+            searchCountryData(country)
         }
     });
 }
